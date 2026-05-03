0cbcbffe41fecf5fd638041929e319c0eec00f9c
Julx127 <julietaromano2008@gmail.com>
2026-05-03 18:46:05 -0300

    Delete MiPrimeraApp directory

--- MiPrimeraApp/app/saludo.tsx
+++
@@ -1,8 +0,0 @@
-import { Text, View } from 'react-native';
-export default function Saludo() {
-return (
-<View>
-<Text style={{ fontSize: 20, color: 'blue' }}>Hola desde otra pantalla </Text>
-</View>
-);
-}
